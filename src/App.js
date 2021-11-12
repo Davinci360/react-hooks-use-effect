96cd731697f08192de315d2d183687b430bf0259
davinci360 <jasonbrowntime@gmail.com>
2021-11-12 06:06:01 -0500

    coplete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    console.log("useEffect called");
-  });
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
 
+  useEffect(
+    // 1st arg: side effect (callback function)
+    () => console.log("useEffect called"),
+    // 2nd arg: dependencies array
+    [count, text]
+  );
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
 }
 
+
 export default App;
+//1 on 1 w Chris 11/11/21 2:15 p.m.
